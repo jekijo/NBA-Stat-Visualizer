4e808d22a304971573c479e1336fbd9c8b13e80b
jekijo <88257653+jekijo@users.noreply.github.com>
2021-10-25 16:06:56 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-import bs4
-
-url = "http://basketball-reference.com/players/j/jamesle01.html"
-result = requests.get(url)
-soup = bs4.BeautifulSoup(result.text, 'lxml')
-test = soup.select('.poptip')
-print(test)
